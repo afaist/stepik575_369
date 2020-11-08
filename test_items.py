d2f720e401091b3c1f39bb1430d609909725baa5
afaist <afaist@gmail.com>
2020-11-08 16:41:30 +0700

    3.6.9 Задание: запуск автотестов для разных языков интерфейса

---
+++ test_items.py
@@ -0,0 +1,15 @@
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_guest_should_see_add_to_basket_button(browser):
+    try:
+        browser.get(link)
+        btn = browser.find_element_by_css_selector(".btn-add-to-basket")
+        assert btn
+        print("!!"*20)
+        print(f"Текст на кнопке: '{btn.text}'")
+        print("!!"*20)
+    except Exception as e:
+        print(e)
+        assert False
+
